fc5e5c49eb363b0851e37126238b3ebe538201bc
Cheng-lian <97796169+Cheng-lian@users.noreply.github.com>
2023-11-08 14:36:31 +0800

    feat:增加移动跳跃等操作和对话场景
    可以跑步，跳跃等操作，可以和游戏中博士进行简单的对话交互

---
+++ Assets/TouchingDIrection.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchingDirection : MonoBehaviour
+{
+    public ContactFilter2D castFilter;
+    public float groundDistance = 0.05f;
+    public float wallDistance = 0.2f;
+    public float ceilingDistance = 0.05f;
+    CapsuleCollider2D touchingCol;
+    Animator animator;
+
+    RaycastHit2D[] groundHits = new RaycastHit2D[5];
+    RaycastHit2D[] wallHits = new RaycastHit2D[5];
+    RaycastHit2D[] ceilingHits = new RaycastHit2D[5];
+
+    [SerializeField]
+    private bool _isGrounded;
+
+    public bool IsGrounded
+    {
+        get
+        {
+            return _isGrounded;
+        }
+        private set
+        {
+            _isGrounded = value;
+            animator.SetBool(AnimationStrings.isGrounded, value);
+        }
+    }
+
+    [SerializeField]
+    private bool _isOnWall;
+
+    public bool IsOnWall
+    {
+        get
+        {
+            return _isOnWall;
+        }
+        private set
+        {
+            _isOnWall = value;
+            animator.SetBool(AnimationStrings.isOnWall, value);
+        }
+    }
+
+    [SerializeField]
+    private bool _isOnCeiling;
+    private Vector2 wallCheckDirection => gameObject.transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+
+    public bool IsOnCeiling
+    {
+        get
+        {
+            return _isOnCeiling;
+        }
+        private set
+        {
+            _isOnCeiling = value;
+            animator.SetBool(AnimationStrings.isOnCeiling, value);
+        }
+    }
+
+    private void Awake()
+    {
+        touchingCol = GetComponent<CapsuleCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        IsGrounded = touchingCol.Cast(Vector2.down, castFilter, groundHits, groundDistance) > 0;
+        IsOnWall = touchingCol.Cast(wallCheckDirection, castFilter, wallHits, wallDistance) > 0;
+        IsOnCeiling = touchingCol.Cast(Vector2.up, castFilter, ceilingHits, ceilingDistance) > 0;
+    }
+
+}
