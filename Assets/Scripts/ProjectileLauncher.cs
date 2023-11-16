6d6066ccd5311aff50cc69063e648d06c7850c0d
Cheng-lian <97796169+Cheng-lian@users.noreply.github.com>
2023-11-16 19:09:53 +0800

    feat: 新增猫猫血条和将蓝条同步到猫猫这边
    - 猫猫可以发射魔法弹，消耗蓝量
    - 猫猫的基本动作进行了更新优化
    - 狗狗的动画也进行了补充

--- Assets/Scripts/ProjectileLauncher.cs
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -4,28 +4,24 @@ using UnityEngine;
 
 public class ProjectileLauncher : MonoBehaviour
 {
-    // 发射点的Transform
     public Transform launchPoint;
-
-    // 抛射物的预制体
     public GameObject projectilePrefab;
 
     private Damageable launcherDamageable;
 
-    // 发射抛射物的方法
+    private void Start()
+    {
+        launcherDamageable = GetComponent<Damageable>();
+    }
+
     public void FireProjectile()
     {
         if (launcherDamageable != null && launcherDamageable.Fir())
         {
-            // 实例化抛射物对象，在发射点位置，并使用抛射物预制体的旋转
             GameObject projectile = Instantiate(projectilePrefab, launchPoint.position, projectilePrefab.transform.rotation);
-
-            // 获取抛射物原始的缩放
             Vector3 origScale = projectile.transform.localScale;
-
-            // 根据发射点的方向设置抛射物的缩放，确保抛射物朝向正确的方向
             projectile.transform.localScale = new Vector3(
-                origScale.x * transform.localScale.x > 0 ? 1 : -1,// 如果发射点方向朝右，则保持原始缩放，否则翻转抛射物
+                origScale.x * launchPoint.parent.transform.localScale.x > 0 ? 1 : -1,
                 origScale.y,
                 origScale.z
                 );
@@ -34,5 +30,6 @@ public class ProjectileLauncher : MonoBehaviour
         {
             Debug.Log("Not enough MP to fire!");
         }
+
     }
 }
