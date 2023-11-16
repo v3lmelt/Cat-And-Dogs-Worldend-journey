81dfb7400d4cf5957ef4fab3b86f1489886d505f
LinShenWuQi <12700138+linshenwuqi@user.noreply.gitee.com>
2023-11-16 14:47:38 +0800

    fix:梯子的逻辑bug
    - 这上面的改动显示也有bug 因为这个是我老早就修了的 现在才commit 改动显示的是我测bug改的脚本

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -5,15 +5,19 @@ using UnityEngine.InputSystem;
 
 public class Ladder : MonoBehaviour
 {
-    PlayerInput Player;
+    
 
     private void OnTriggerStay2D(Collider2D collision)
    
     {
-        Player = collision.GetComponent<PlayerInput>();
-        if(Input.GetKey(KeyCode.UpArrow))
+        
+        if(Input.GetKey(KeyCode.UpArrow) && collision.gameObject.name == "Cat")
         {
             collision.GetComponent<Rigidbody2D>().velocity = new Vector3(0,10,0);
+        }
+        if (Input.GetKey(KeyCode.W) && collision.gameObject.name == "Dog")
+        {
+            collision.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 10, 0);
         }
     }
 }
