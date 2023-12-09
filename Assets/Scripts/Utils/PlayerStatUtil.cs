02bb9317fbc31a9caa425be472f72addc41c79d7
LinShenWuQi <12700138+linshenwuqi@user.noreply.gitee.com>
2023-12-09 23:20:39 +0800

    fix; 修正狗狗攻击范围

--- Assets/Scripts/Utils/PlayerStatUtil.cs
+++ Assets/Scripts/Utils/PlayerStatUtil.cs
@@ -5,11 +5,12 @@ public static class PlayerStatUtil
     private static PlayerController[] _playerControllers = new PlayerController[2];
     private static Attack[] _playerAttacks = new Attack[2];
     private static Damageable[] _playerDamageables = new Damageable[2];
+    private static ProjectileLauncher _dogMagicPower;
     
     public static readonly string[] SceneExcludeFromGettingComponents = new string[]{"StartMenu", "Background Story",};
     public static readonly string[] SceneExcludeFromStatRestore = new string[]{"StartMenu", "Background Story", "GameplayScene",
         "Level1"};
-    public static readonly string[] SceneExcludeFromStatRecord = new string[]{"StartMenu", "Background Story", "GameplayScene" };
+    public static readonly string[] SceneExcludeFromStatRecord = new string[]{"StartMenu", "Background Story" };
     private struct CatStats
     {
         public static int CurrentHealth;
@@ -30,6 +31,7 @@ public static class PlayerStatUtil
         public static int MaxMp;
 
         public static int AttackDamage;
+        public static int MagicDamage;  
     }
     
         public static void IncreasePlayerHp(int increaseAmount)
@@ -52,9 +54,9 @@ public static class PlayerStatUtil
         {
             foreach (var d in _playerAttacks)
             {
-                d.attackDamage += increaseAmount;
-                GameManager.Instance.dog.gameObject.GetComponent<ProjectileLauncher>().MagicDamage += increaseAmount;
+                d.attackDamage += increaseAmount; 
             }
+            GameManager.Instance.dog.gameObject.GetComponent<ProjectileLauncher>().MagicDamage += increaseAmount;
         }
         public static void IncreaseRestoreMp(int increaseAmount)
          {
@@ -73,6 +75,7 @@ public static class PlayerStatUtil
             _playerAttacks[1] = GameManager.Instance.dog.GetComponentInChildren<Attack>();
             _playerDamageables[0] = GameManager.Instance.cat.GetComponent<Damageable>();
             _playerDamageables[1] = GameManager.Instance.dog.GetComponent<Damageable>();
+            _dogMagicPower = GameManager.Instance.dog.GetComponent<ProjectileLauncher>();
         }
 
         public static void RecordPlayerStats()
@@ -89,6 +92,7 @@ public static class PlayerStatUtil
              DogStats.CurrentMp = _playerDamageables[1].MP;
              DogStats.MaxMp = _playerDamageables[1].MaxMP;
              DogStats.AttackDamage = _playerAttacks[1].attackDamage;
+             DogStats.MagicDamage = _dogMagicPower.MagicDamage;
         }
 
         public static void RestorePlayerStats()
@@ -104,5 +108,6 @@ public static class PlayerStatUtil
             _playerDamageables[1].MP = DogStats.CurrentMp;
             _playerDamageables[1].MaxMP = DogStats.MaxMp;
             _playerAttacks[1].attackDamage = DogStats.AttackDamage;
-        }
+             _dogMagicPower.MagicDamage = DogStats.MagicDamage;
+    }
     }
