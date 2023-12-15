4fcf324ba4a52c70b9537951bbccc05bff147b07
v3lmelt <143810559+v3lmelt@users.noreply.github.com>
2023-12-15 15:08:49 +0800

    fix: 对单元测试进行了修复

--- Assets/Tests/EditorTest/EditorTestConst.cs
+++ Assets/Tests/EditorTest/EditorTestConst.cs
@@ -5,7 +5,8 @@
         public static readonly string[] SceneExcludeFromSetupTest = new string[]
         {
             "Background Story.unity", "FunctionTest.unity", "GameplayScene.unity",
-            "StartMenu.unity", "TestScene.unity", "UnitTestScene.unity"
+            "StartMenu.unity", "TestScene.unity", "UnitTestScene.unity",
+            "Japan.unity", "Japan_1.unity", "Japan_2.unity", "Germant_BeforeBoss.unity", "Germant_Boss.unity"
         };
     }
 }
